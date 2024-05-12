f161f194c68d85f467e94f57d9396fba30ee192a
maxWoosh <140332381+maxWoosh@users.noreply.github.com>
2024-05-12 18:10:56 +0300

    Update program.cs

--- fa/program.cs
+++ fa/program.cs
@@ -15,28 +15,172 @@ namespace fans
 
 
   public class FA1
-  {
-    public bool? Run(IEnumerable<char> s)
+{
+    public static State a = new State()
     {
-      return false;
-    }
-  }
+        Name = "a",
+        IsAcceptState = false,
+        Transitions = new Dictionary<char, State>()
+    };
 
-  public class FA2
-  {
+    public State b = new State()
+    {
+        Name = "b",
+        IsAcceptState = false,
+        Transitions = new Dictionary<char, State>()
+    };
+
+    public State c = new State()
+    {
+        Name = "c",
+        IsAcceptState = false,
+        Transitions = new Dictionary<char, State>()
+    };
+
+    public State d = new State()
+    {
+        Name = "d",
+        IsAcceptState = true,
+        Transitions = new Dictionary<char, State>()
+    };
+
+    State InitialState = a;
+
+    public FA1()
+    {
+        a.Transitions['0'] = b;
+        a.Transitions['1'] = a;
+        b.Transitions['0'] = c;
+        b.Transitions['1'] = b;
+        c.Transitions['0'] = c;
+        c.Transitions['1'] = d;
+        d.Transitions['0'] = c;
+        d.Transitions['1'] = d;
+    }
+
     public bool? Run(IEnumerable<char> s)
     {
-      return false;
+        State current = InitialState;
+        foreach (var c in s)
+        {
+            if (!current.Transitions.ContainsKey(c))
+            {
+                return null; // вернуть null в случае отсутствия перехода
+            }
+            current = current.Transitions[c];
+        }
+        return current.IsAcceptState;
     }
-  }
-  
-  public class FA3
-  {
+}
+
+public class FA2
+{
+    public static State q0 = new State()
+    {
+        Name = "q0",
+        IsAcceptState = false,
+        Transitions = new Dictionary<char, State>()
+    };
+
+    public State q1 = new State()
+    {
+        Name = "q1",
+        IsAcceptState = false,
+        Transitions = new Dictionary<char, State>()
+    };
+
+    public State q2 = new State()
+    {
+        Name = "q2",
+        IsAcceptState = true,
+        Transitions = new Dictionary<char, State>()
+    };
+
+    State InitialState = q0;
+
+    public FA2()
+    {
+        q0.Transitions['0'] = q1;
+        q0.Transitions['1'] = q0;
+        q1.Transitions['0'] = q2;
+        q1.Transitions['1'] = q0;
+        q2.Transitions['0'] = q1;
+        q2.Transitions['1'] = q2;
+    }
+
     public bool? Run(IEnumerable<char> s)
     {
-      return false;
+        State current = InitialState;
+        foreach (var c in s)
+        {
+            if (!current.Transitions.ContainsKey(c))
+            {
+                return null; // вернуть null в случае отсутствия перехода
+            }
+            current = current.Transitions[c];
+        }
+        return current.IsAcceptState;
     }
-  }
+}
+
+public class FA3
+{
+    public static State q0 = new State()
+    {
+        Name = "q0",
+        IsAcceptState = false,
+        Transitions = new Dictionary<char, State>()
+    };
+
+    public State q1 = new State()
+    {
+        Name = "q1",
+        IsAcceptState = false,
+        Transitions = new Dictionary<char, State>()
+    };
+
+    public State q2 = new State()
+    {
+        Name = "q2",
+        IsAcceptState = false,
+        Transitions = new Dictionary<char, State>()
+    };
+
+    public State q3 = new State()
+    {
+        Name = "q3",
+        IsAcceptState = true,
+        Transitions = new Dictionary<char, State>()
+    };
+
+    State InitialState = q0;
+
+    public FA3()
+    {
+        q0.Transitions['0'] = q0;
+        q0.Transitions['1'] = q1;
+        q1.Transitions['0'] = q0;
+        q1.Transitions['1'] = q2;
+        q2.Transitions['0'] = q0;
+        q2.Transitions['1'] = q3;
+        q3.Transitions['0'] = q0;
+        q3.Transitions['1'] = q3;
+    }
+
+    public bool? Run(IEnumerable<char> s)
+    {
+        State current = InitialState;
+        foreach (var c in s)
+        {
+            if (!current.Transitions.ContainsKey(c))
+            {
+                return null; // вернуть null в случае отсутствия перехода
+            }
+            current = current.Transitions[c];
+        }
+        return current.IsAcceptState;
+    }
+}
 
   class Program
   {
